72ae1eef0b067f6f04949b295722716171e1713e
ayoubsofi <ayoub@email.com>
2026-04-27 17:59:12 +0100

    create structure of teacher tasks

--- scripts/database.php
+++ scripts/database.php
@@ -8,6 +8,7 @@ $conn = "";
 
 try{
     $conn = new PDO("mysql:host=$db_server;dbname=$db_name", $db_user, $db_pass);
+    // echo'connected with succefully';
 }catch(PDOException $e){
     echo 'error'.$e->getMessage();
 }
